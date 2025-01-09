193eb3248947f80d1d3b6674d66ae888440cb3d3
AmitabhVerma26 <112395689+AmitabhVerma26@users.noreply.github.com>
2025-01-09 13:12:10 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,4 +2,4 @@ module.exports = {
   url: process.env.DB_URL,
 };
 
-// mongodb+srv://hoonar:gurgaon21@cluster0.zi2qyxd.mongodb.net/hoonar?retryWrites=true&w=majority
+// mongodb+srv://hoonar:gurgaon21@cluster0.zi2qyxd.mongodb.net?retryWrites=true&w=majority&appName=Cluster0
